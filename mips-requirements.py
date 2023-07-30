070094429c2c88129381a8200d95c70496c0979e
Colin Nolan <cn580@alumni.york.ac.uk>
2023-07-30 13:37:43 +0100

    Development of deploy script.

--- mips-requirements.py
+++ mips-requirements.py
@@ -1,14 +1,42 @@
+import argparse
+
 import mip
 
-mip.install("abc")
-mip.install("collections-defaultdict")
-mip.install("datetime")
-mip.install("functools")
-mip.install("logging")
-mip.install("threading")
-mip.install("github:pfalcon/pycopy-lib/typing/typing.py")
 
-# Required by microwebsrv2
-mip.install("github:pfalcon/pycopy-lib/ffilib/ffilib.py")
-mip.install("github:pfalcon/pycopy-lib/select/select.py")
-mip.install("github:pfalcon/pycopy-lib/socket/socket.py")
+LIBRARIES_TO_INSTALL = (
+    "abc",
+    "collections-defaultdict",
+    "datetime",
+    "functools",
+    "logging",
+    "threading",
+    "github:pfalcon/pycopy-lib/typing/typing.py",
+
+    # Required by microwebsrv2
+    "github:pfalcon/pycopy-lib/ffilib/ffilib.py", 
+    "github:pfalcon/pycopy-lib/select/select.py",
+    "github:pfalcon/pycopy-lib/socket/socket.py",
+)
+
+
+def _install_libs(install_args: tuple[str | tuple[str, dict], ...], install_location: str):
+    for args in install_args:
+        kwargs = dict(target=install_location, mpy=False)
+        if isinstance(args, dict):
+            kwargs = args[1] | kwargs
+            args = args[0]
+
+        mip.install(args, **kwargs)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('install_location', help='The location where the libs will be installed')
+
+    args = parser.parse_args()
+    install_location = args.install_location
+    _install_libs(LIBRARIES_TO_INSTALL, install_location)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,12 @@
 
 set -eu -o pipefail
 
+architecture="${1:-x64}"
+
 script_directory="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 project_directory="$(cd "${script_directory}/.." > /dev/null 2>&1 && pwd)"
 build_directory="${project_directory}/build"
+lib_install_directory="${build_directory}/lib"
 
 rm -rf "${build_directory}"
 mkdir -p "${build_directory}"
@@ -15,12 +18,27 @@ pushd "${project_directory}" > /dev/null
 poetry build --format wheel
 cp dist/*.whl "${build_directory}"
 
->&2 echo "Installing into venv..."
-python3 -m venv "${build_directory}/venv"
-"${build_directory}/venv/bin/pip" install -t "${build_directory}/install" "${build_directory}"/*.whl
+>&2 echo "Downloading Poetry requirements..."
+pip3 install -t "${lib_install_directory}" "${build_directory}"/*.whl
+rm -rf "${lib_install_directory}"/*.dist-info
 
->&2 echo "Preparing install..."
-rm -rf "${build_directory}/install"/*.dist-info
+>&2 echo "Downloading mip requirements..."
+docker run --rm \
+    -v "${lib_install_directory}:/install" \
+    -v "${project_directory}/mips-requirements.py:/mips-requirements.py:ro" \
+    micropython/unix:latest \
+    micropython /mips-requirements.py /install
+# MicroPython complained about permission issues when running as non-root user so correcting file ownership in next step
+docker run --rm \
+    -v "${lib_install_directory}:/install" \
+    alpine:latest \
+    chown -R "$(id -u):$(id -g)" /install
 
+>&2 echo "Pre-compiling libs..."
+# Find py files, compile them, and remove the original
+find "${lib_install_directory}" -name "*.py" -type f \
+    -exec sh -c "mpy-cross -march=\"${architecture}\" \"\$0\"; rm \"\$0\"" {} \;
 
 popd > /dev/null
+
+>&2 echo "Complete!"
