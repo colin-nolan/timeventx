68a91844023d16a7e399a86bf97a077b70e8dee0
Colin Nolan <cn580@alumni.york.ac.uk>
2023-10-09 13:35:46 +0100

    Adds tests against web server.

--- backend/garden_water/tests/test_web_server.py
+++ backend/garden_water/tests/test_web_server.py
@@ -1,6 +1,10 @@
 import logging
+import os
+import tempfile
 from copy import deepcopy
+from pathlib import Path
 from socket import socket
+from unittest.mock import patch
 
 import pytest
 import requests
@@ -36,9 +40,14 @@ def database() -> IdentifiableTimersCollection:
 
 
 @pytest.fixture
-def api_test_client(database: IdentifiableTimersCollection) -> TestClient:
+def configuration() -> Configuration:
+    return Configuration()
+
+
+@pytest.fixture
+def api_test_client(database: IdentifiableTimersCollection, configuration: Configuration) -> TestClient:
     test_app = deepcopy(app)
-    test_app.configuration = Configuration()
+    test_app.configuration = configuration
     test_app.database = database
     return TestClient(test_app)
 
@@ -94,3 +103,69 @@ async def test_post_timer_no_payload(api_test_client: TestClient):
 async def test_post_timer_wrong_json_properties(api_test_client: TestClient):
     response = await api_test_client.post(f"/api/{API_VERSION}/timer", body={"foo": "bar"})
     assert response.status_code == 400, response.text
+
+
+@pytest.mark.asyncio
+async def test_post_timer_invalid_json(api_test_client: TestClient):
+    response = await api_test_client.post(f"/api/{API_VERSION}/timer", body="{{nope}}")
+    assert response.status_code == 400, response.text
+
+
+@pytest.mark.asyncio
+async def test_put_timer(api_test_client: TestClient):
+    response = await api_test_client.put(
+        f"/api/{API_VERSION}/timer/{EXAMPLE_IDENTIFIABLE_TIMER_1.id}", body=timer_to_json(EXAMPLE_IDENTIFIABLE_TIMER_1)
+    )
+    assert response.status_code == 201, response.text
+
+
+@pytest.mark.asyncio
+async def test_delete_timer(api_test_client: TestClient, database: IdentifiableTimersCollection):
+    database.add(EXAMPLE_IDENTIFIABLE_TIMER_1)
+    response = await api_test_client.delete(f"/api/{API_VERSION}/timer/{EXAMPLE_IDENTIFIABLE_TIMER_1.id}")
+    assert response.status_code == 200, response.text
+
+
+@pytest.mark.asyncio
+async def test_delete_timer_that_does_not_exist(api_test_client: TestClient):
+    response = await api_test_client.delete(f"/api/{API_VERSION}/timer/{EXAMPLE_IDENTIFIABLE_TIMER_1.id}")
+    assert response.status_code == 404, response.text
+
+
+@pytest.mark.asyncio
+async def test_serve_root(api_test_client: TestClient, configuration: Configuration):
+    with tempfile.TemporaryDirectory() as temp_directory:
+        with open(os.path.join(temp_directory, "index.html"), "w") as index_file:
+            index_file.write("hello")
+
+        with patch.dict(os.environ, {Configuration.FRONTEND_ROOT_DIRECTORY.environment_variable_name: temp_directory}):
+            response = await api_test_client.get(f"/")
+    assert response.status_code == 200, response.text
+    assert response.text == "hello"
+    assert response.headers["Content-Type"] == "text/html"
+
+
+@pytest.mark.asyncio
+async def test_serve_file(api_test_client: TestClient, configuration: Configuration):
+    with tempfile.TemporaryDirectory() as temp_directory:
+        Path(temp_directory, "test.jpg").touch()
+
+        with patch.dict(os.environ, {Configuration.FRONTEND_ROOT_DIRECTORY.environment_variable_name: temp_directory}):
+            response = await api_test_client.get(f"/test.jpg")
+    assert response.status_code == 200, response.text
+    assert response.headers["Content-Type"] == "image/jpeg"
+
+
+@pytest.mark.asyncio
+async def test_serve_outside_frontend_directory(api_test_client: TestClient, configuration: Configuration):
+    with tempfile.TemporaryDirectory() as temp_directory_outer:
+        temp_directory = f"{temp_directory_outer}/inner"
+        os.makedirs(temp_directory)
+        Path(temp_directory_outer, "secret").touch()
+
+        relative_secret_path = "../secret"
+        assert Path(temp_directory, relative_secret_path).exists()
+
+        with patch.dict(os.environ, {Configuration.FRONTEND_ROOT_DIRECTORY.environment_variable_name: temp_directory}):
+            response = await api_test_client.get(f"/{relative_secret_path}")
+    assert response.status_code == 404, response.text
