b1b22ab9bccb6fbe284dc03fe37a03f0752a8627
Colin Nolan <cn580@alumni.york.ac.uk>
2023-10-10 23:59:34 +0100

    Run frontend tests against backend.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -69,9 +69,23 @@ export default defineConfig({
     ],
 
     /* Run your local dev server before starting the tests */
-    webServer: {
-        command: "yarn run dev --port 3003",
-        url: "http://127.0.0.1:3003",
-        reuseExistingServer: !process.env.CI,
-    },
+    webServer: [
+        {
+            command: "VITE_BACKEND_API_ROOT=http://127.0.0.1:3004/api/v1 yarn run dev --port 3003",
+            url: "http://127.0.0.1:3003",
+            reuseExistingServer: !process.env.CI,
+        },
+        {
+            command:
+                "GARDEN_WATER_TIMERS_DATABASE_LOCATION=/tmp/test.db " +
+                "GARDEN_WATER_BACKEND_PORT=3004 " +
+                "GARDEN_WATER_INTERFACE=127.0.0.1 " +
+                "GARDEN_WATER_RESTART_ON_ERROR=false " +
+                "GARDEN_WATER_LOG_LEVEL=10 " +
+                "PYTHONPATH=../backend " +
+                "python ../backend/garden_water/main.py",
+            url: "http://127.0.0.1:3004/api/v1/healthcheck",
+            reuseExistingServer: !process.env.CI,
+        },
+    ],
 });
